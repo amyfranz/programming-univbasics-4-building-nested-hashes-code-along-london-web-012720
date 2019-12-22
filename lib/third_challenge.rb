75019c8ef2da376acff7705cf928e8eacec66a4a
Amy Franz <amy.franz@gmail.com>
2019-12-22 19:14:51 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -7,7 +7,7 @@ def third_challenge
         },
       :matriarch => {
         :name => "Lady Montague",
-        :age of "54"
+        :age => "54"
       },
       :hero => {
         :name => "Romeo",
